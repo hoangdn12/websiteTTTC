cb1c0c0ed3be81b03a3205d592f1f79619f448e1
viethoang <viethoang212001@gmail.com>
2022-06-26 08:31:57 +0700

    first commit

---
+++ includes/myaccount-sidebar.php
@@ -0,0 +1,20 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">Tiến độ thanh toán của bạn</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">Tài khoản của tôi</a></li>
+					<li><a href="bill-ship-addresses.php">Địa chỉ giao hàng / thanh toán</a></li>
+					<li><a href="order-history.php">lịch sử đơn hàng</a></li>
+					<li><a href="pending-orders.php">Đơn đặt hàng đang chờ thanh toán</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
